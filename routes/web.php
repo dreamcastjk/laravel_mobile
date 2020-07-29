7287e9e6b59a60e676322be36bf464e12266a544
Zhiharev <slipdoll@gmail.com>
2020-07-29 13:42:26 +0300

    added base templates

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Auth::routes();
+Route::get('/categories', function () {
+    return view('categories');
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/mobiles/iphone_x_64', function () {
+    return view('product');
+});
