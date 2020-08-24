600c1ed9054fe3750280026853cbd021f717744b
Zhiharev <slipdoll@gmail.com>
2020-08-24 18:01:00 +0300

    p14 - form requests

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -23,12 +23,18 @@ class ProductRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'code' => ['required', 'min:3', 'max:255'],
+        $rules = [
+            'code' => ['required', 'min:3', 'max:255', 'unique:categories,code'],
             'name' => ['required', 'min:3', 'max:255'],
             'description' => ['required', 'min:5'],
             'price' => ['required', 'numeric', 'min:1']
         ];
+
+        if ($this->route()->named('categories.update')) {
+            $rules['code'][3] .= ", " . $this->route()->parameter('category')->id;
+        }
+
+        return $rules;
     }
 
     public function messages()
